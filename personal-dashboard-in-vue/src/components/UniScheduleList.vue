fae59095f48ec9856ad9095328910543646f2c83
hymroh2 <hymroh091403@gmail.com>
2024-02-25 16:37:18 +0900

    created UniScheduleList component (work in progress)

---
+++ personal-dashboard-in-vue/src/components/UniScheduleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ScheduleItem />
+  </div>
+</template>
+
+<script lang="ts">
+  import ScheduleItem from '@/components/ScheduleItem.vue';
+
+  export default {
+    name: 'UniScheduleList',
+    components: {
+      ScheduleItem
+    }
+  }
+</script>
