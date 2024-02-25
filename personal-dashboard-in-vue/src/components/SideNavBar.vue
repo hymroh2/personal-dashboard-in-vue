19ef350178be9d8d1be7ba5dc81fd492b63eaf7e
hymroh2 <hymroh091403@gmail.com>
2024-02-25 16:04:33 +0900

    created SideNavBar component

---
+++ personal-dashboard-in-vue/src/components/SideNavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fixed h-screen bg-gray-600 max-w-64 w-full">
+    <h1>My App</h1>
+    <RouterLink to="/" class="block">Home</RouterLink>
+    <RouterLink to="/about" class="block">About</RouterLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { RouterLink } from 'vue-router';
+
+export default {
+  name: "SideNavBar",
+  components: {
+    RouterLink
+  }
+}
+</script>
+
+<style></style>
