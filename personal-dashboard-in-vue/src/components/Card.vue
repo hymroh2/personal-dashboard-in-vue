4b0a3aaa27662188804c430b1d56947aedb59ddb
hymroh2 <hymroh091403@gmail.com>
2024-02-25 16:10:20 +0900

    created card component

---
+++ personal-dashboard-in-vue/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="max-w-sm m-5 bg-white rounded-xl border overflow-hidden shadow-xl">
+    <div class="px-6 py-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Card'
+}
+</script>
