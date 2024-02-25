3a564f04a6961d32e01a9dadb303526ce8a151eb
hymroh2 <hymroh091403@gmail.com>
2024-02-25 16:18:57 +0900

    created ScheduleItem component

---
+++ personal-dashboard-in-vue/src/components/ScheduleItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ course_code }}</h1>
+    <p>{{ class_type }}</p>
+    <p>{{ time }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ScheduleItem',
+  props: {
+    id: String,
+    course_code: String,
+    class_type: String,
+    time: String
+  }
+}
+</script>
+
+<style>
+</style>
