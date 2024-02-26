9c078d6f13d8751902e1d2a3866cfa0f69289290
hymroh2 <hymroh091403@gmail.com>
2024-02-26 16:52:08 +0900

    created getScheduleToday getter, added typescript typing to the state for further safety

--- personal-dashboard-in-vue/src/stores/UniScheduleStore.ts
+++ personal-dashboard-in-vue/src/stores/UniScheduleStore.ts
@@ -1,7 +1,25 @@
 import { defineStore } from "pinia";
+import type { UniScheduleItem } from "@/types/UniScheduleTypes";
 
 export const useUniScheduleStore = defineStore('uniScheduleStore', {
   state: () => ({
-    schedule: []
-  })
+    schedule: [] as UniScheduleItem[]
+  }),
+  getters: {
+    getScheduleToday: (state) => {
+      const today: Number = (new Date()).getDay();
+      
+      let newState: UniScheduleItem[] = [];
+
+      function compareStartTime(a: UniScheduleItem, b: UniScheduleItem) {
+        return Number(a.start_time) - Number(b.start_time);
+      }
+
+      if(state.schedule.length > 0) {
+        newState = state.schedule.filter((session) => session.day === today).sort(compareStartTime);
+      }
+
+      return newState;
+    }
+  }
 })
