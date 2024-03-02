56985b3fe84676be8f133ddf754504f3a0ca11cc
hymroh2 <hymroh091403@gmail.com>
2024-03-02 13:56:47 +0900

    added controller to the state, added getScheduleAll, getController getters and fetchData action

--- personal-dashboard-in-vue/src/stores/UniScheduleStore.ts
+++ personal-dashboard-in-vue/src/stores/UniScheduleStore.ts
@@ -1,25 +1,37 @@
 import { defineStore } from "pinia";
 import type { UniScheduleItem } from "@/types/UniScheduleTypes";
+import UniScheduleController from "@/controllers/UniScheduleController";
 
 export const useUniScheduleStore = defineStore('uniScheduleStore', {
   state: () => ({
-    schedule: [] as UniScheduleItem[]
+    schedule: [] as UniScheduleItem[],
+    controller: new UniScheduleController()
   }),
   getters: {
+    getScheduleAll: (state) => {
+      return state.schedule;
+    },
     getScheduleToday: (state) => {
       const today: Number = (new Date()).getDay();
-      
-      let newState: UniScheduleItem[] = [];
+
+      let schedule: UniScheduleItem[] = [];
 
       function compareStartTime(a: UniScheduleItem, b: UniScheduleItem) {
-        return Number(a.start_time) - Number(b.start_time);
+        return a.start_time - b.start_time;
       }
 
-      if(state.schedule.length > 0) {
-        newState = state.schedule.filter((session) => session.day === today).sort(compareStartTime);
+      if (state.schedule.length > 0) {
+        schedule = state.schedule.filter((session) => session.day === today).sort(compareStartTime);
       }
-
-      return newState;
+      return schedule;
+    },
+    getController: (state) => {
+      return state.controller;
+    }
+  },
+  actions: {
+    async fetchData() {
+      this.schedule = await this.controller.getData()
     }
   }
 })
