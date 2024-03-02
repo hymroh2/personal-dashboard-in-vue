68a31391d3378d1c9eb3c19f8c147c8f468042e7
hymroh2 <hymroh091403@gmail.com>
2024-03-02 13:57:54 +0900

    added conditional rendering based on the uni schedule

--- personal-dashboard-in-vue/src/components/UniScheduleList.vue
+++ personal-dashboard-in-vue/src/components/UniScheduleList.vue
@@ -1,7 +1,9 @@
 <template>
-  <div v-for="item in uniScheduleStore.getScheduleToday">
+  <p>Uni Schedule</p>
+  <div v-if="uniScheduleStore.getScheduleToday.length > 0" v-for="item in uniScheduleStore.getScheduleAll">
     <ScheduleItem :id="item.id" :course_code="item.course_id" :class_type="item.class_type" :time="item.start_time" />
   </div>
+  <p v-else>No Classes Today</p>
 </template>
 
 <script lang="ts">
@@ -15,6 +17,7 @@
     },
     setup() {
       const uniScheduleStore = useUniScheduleStore();
+      uniScheduleStore.fetchData();
       return { uniScheduleStore };
     }
   }
